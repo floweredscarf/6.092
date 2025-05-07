767f15c0dbcd312abcd54cf6cd01f32ceca434b0
floweredscarf <2460753970@qq.com>
2025-05-07 16:39:57 +0800

    perf: put BouncingBox in an ArrayList

--- Graphics/DrawGraphics.java
+++ Graphics/DrawGraphics.java
@@ -1,27 +1,27 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class DrawGraphics {
-    BouncingBox box;
-    BouncingBox box1;
-    BouncingBox box2;
+    ArrayList<BouncingBox> boxes = new ArrayList<BouncingBox>();
     
     /** Initializes this class for drawing. */
     public DrawGraphics() {
-        box = new BouncingBox(200, 50, Color.RED);
-        box.setMovementVector(1, 0);
-        box1 = new BouncingBox(200, 50, Color.BLUE);
-        box1.setMovementVector(0, -2);
-        box2 = new BouncingBox(200, 50, Color.GREEN);
-        box2.setMovementVector(1, -2);
+        boxes.add(new BouncingBox(200, 50, Color.RED));
+        boxes.get(0).setMovementVector(1, 0);
+        boxes.add(new BouncingBox(200, 50, Color.BLUE));
+        boxes.get(1).setMovementVector(0, -2);
+        boxes.add(new BouncingBox(200, 50, Color.GREEN));
+        boxes.get(2).setMovementVector(1, -2);
     }
 
     /** Draw the contents of the window on surface. Called 20 times per second. */
     public void draw(Graphics surface) {
         surface.drawLine(50, 50, 250, 250);
-        box.draw(surface);
-        box1.draw(surface);
-        box2.draw(surface);
+        for (BouncingBox box : boxes)
+        {
+            box.draw(surface);
+        }
         surface.drawRect(100, 100, 50, 50);
         surface.drawArc(150, 150, 50, 50, 0, 360);
         surface.drawString("text", 50, 50);
