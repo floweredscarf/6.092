ac39471e1a759045160f9f3cf27df99320e29177
floweredscarf <2460753970@qq.com>
2025-05-09 16:53:30 +0800

    perf: change to contain an ArrayList, add a second Rectangle

--- assn06/DrawGraphics.java
+++ assn06/DrawGraphics.java
@@ -1,18 +1,25 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class DrawGraphics {
-    Bouncer movingSprite;
+    ArrayList<Bouncer> movingSprites = new ArrayList<Bouncer>();
 
     /** Initializes this class for drawing. */
     public DrawGraphics() {
         Rectangle box = new Rectangle(15, 20, Color.RED);
-        movingSprite = new Bouncer(100, 170, box);
-        movingSprite.setMovementVector(3, 1);
+        movingSprites.add(new Bouncer(100, 170, box));
+        movingSprites.get(0).setMovementVector(3, 1);
+        Rectangle box1 = new Rectangle(15, 20, Color.BLUE);
+        movingSprites.add(new Bouncer(100, 170, box1));
+        movingSprites.get(1).setMovementVector(-3, 1);
     }
 
     /** Draw the contents of the window on surface. */
     public void draw(Graphics surface) {
-        movingSprite.draw(surface);
+        for (Bouncer movingSprite : movingSprites)
+        {
+            movingSprite.draw(surface);
+        }
     }
 }
