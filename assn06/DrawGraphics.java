5e27e9aa2837004ac4654305d3f52047a9ce1fb2
floweredscarf <2460753970@qq.com>
2025-05-11 00:18:25 +0800

    perf: create Mover interface

--- assn06/DrawGraphics.java
+++ assn06/DrawGraphics.java
@@ -3,32 +3,27 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class DrawGraphics {
-    ArrayList<Bouncer> movingSprites = new ArrayList<Bouncer>();
-    ArrayList<StraightMover> straightMovers = new ArrayList<StraightMover>();
+    ArrayList<Mover> movers = new ArrayList<Mover>();
 
     /** Initializes this class for drawing. */
     public DrawGraphics() {
         Rectangle box = new Rectangle(15, 20, Color.RED);
-        movingSprites.add(new Bouncer(100, 170, box));
-        movingSprites.get(0).setMovementVector(3, 1);
-        straightMovers.add(new StraightMover(100, 170, box));
-        straightMovers.get(0).setMovementVector(3, -1);
+        movers.add(new Bouncer(100, 170, box));
+        movers.get(0).setMovementVector(3, 1);
+        movers.add(new StraightMover(100, 170, box));
+        movers.get(1).setMovementVector(3, -1);
         RoundRectangle box1 = new RoundRectangle(15, 20, 5, 5, Color.BLUE);
-        movingSprites.add(new Bouncer(100, 170, box1));
-        movingSprites.get(1).setMovementVector(-3, 1);
-        straightMovers.add(new StraightMover(100, 170, box1));
-        straightMovers.get(1).setMovementVector(-3, -1);
+        movers.add(new Bouncer(100, 170, box1));
+        movers.get(2).setMovementVector(-3, 1);
+        movers.add(new StraightMover(100, 170, box1));
+        movers.get(3).setMovementVector(-3, -1);
     }
 
     /** Draw the contents of the window on surface. */
     public void draw(Graphics surface) {
-        for (Bouncer movingSprite : movingSprites)
+        for (Mover mover : movers)
         {
-            movingSprite.draw(surface);
-        }
-        for (StraightMover straightMover : straightMovers)
-        {
-            straightMover.draw(surface);
+            mover.draw(surface);
         }
     }
 }
