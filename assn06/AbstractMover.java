b8b50430804faacf736a6d91381d5fda571df0b0
floweredscarf <2460753970@qq.com>
2025-05-11 12:31:41 +0800

    perf: reduce duplication by putting common code into AbstractMover class

---
+++ assn06/AbstractMover.java
@@ -0,0 +1,29 @@
+import java.awt.Graphics;
+
+public abstract class AbstractMover implements Mover {
+    protected int x;
+    protected int y;
+    protected int xDirection;
+    protected int yDirection;
+    protected Sprite sprite;
+
+    public AbstractMover(int startX, int startY, Sprite spirte)
+    {
+        x = startX;
+        y = startY;
+        this.sprite = spirte;
+    }
+
+    public void setMovementVector(int xIncrement, int yIncrement)
+    {
+        xDirection = xIncrement;
+        yDirection = yIncrement;
+    }
+
+    public void draw(Graphics surface)
+    {
+        sprite.draw(surface, x, y);
+        x += xDirection;
+        y += yDirection;
+    }
+}
--- assn06/StraightMover.java
+++ assn06/StraightMover.java
@@ -1,31 +1,6 @@
-import java.awt.Graphics;
-
-
-public class StraightMover implements Mover{
-    private int x;
-    private int y;
-    private int xDirection;
-    private int yDirection;
-    private Sprite sprite;
-
+public class StraightMover extends AbstractMover {
     /** Create a StraightMover that positions sprite at (startX, startY). */
     public StraightMover(int startX, int startY, Sprite sprite) {
-        x = startX;
-        y = startY;
-        this.sprite = sprite; 
-    }
-
-    /** Starts moving the object in the direction (xIncrement, yIncrement). */
-    public void setMovementVector(int xIncrement, int yIncrement) {
-        xDirection = xIncrement;
-        yDirection = yIncrement;
-    }
-
-    public void draw(Graphics graphics) {
-        sprite.draw(graphics, x, y);
-
-        // Move the center of the object each time we draw it
-        x += xDirection;
-        y += yDirection;
+        super(startX, startY, sprite);
     }
 }
